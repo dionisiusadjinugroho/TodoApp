508d88c0342e282efeef1a9189f370f7c4607c9a
Dionisius Adji Nugroho <58067755+dionisiusadjinugroho@users.noreply.github.com>
2024-11-15 22:35:26 +0700

    Add ToDo.db

--- TodoWebAPI/TodoWebAPI/obj/Debug/net8.0/TodoWebAPI.AssemblyInfo.cs
+++ TodoWebAPI/TodoWebAPI/obj/Debug/net8.0/TodoWebAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+597512486c8ee88ebe82ba9c6ece8f24acacdb00")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
